3d0ac3f85fc8baa0103ae75096ad7255fbc3cafa
Rayan Encarnacion <rayanbalbuena@gmail.com>
2022-07-11 21:00:27 -0400

    [A] Login page to access directly with github

---
+++ pages/login/index.tsx
@@ -0,0 +1,12 @@
+import { signIn } from "next-auth/react";
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <button onClick={() => signIn("github", { callbackUrl: "/" })}>
+        Signin
+      </button>
+    </div>
+  );
+}
