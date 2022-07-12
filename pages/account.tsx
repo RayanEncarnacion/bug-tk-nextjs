bb103d6b5ef1e49f6d35316966f883b4b0e3d567
Rayan Encarnacion <rayanbalbuena@gmail.com>
2022-07-11 20:58:06 -0400

    [A] Pages to test auth permissions

---
+++ pages/account.tsx
@@ -0,0 +1,26 @@
+import { getSession } from "next-auth/react";
+import { GetServerSideProps } from "next";
+import sessionAdapter from "adapters/sessionAdapter";
+
+export default function Account() {
+  return <div>Esta logeado!</div>;
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  const adaptedSession = sessionAdapter(session);
+
+  return {
+    props: { session: adaptedSession },
+  };
+};
