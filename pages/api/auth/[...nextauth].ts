ae7b2b21d233c11c5107e48541b11625d0ccab3b
Rayan Encarnacion <rayanbalbuena@gmail.com>
2022-07-17 22:18:48 -0400

    [A] Added credentials login with email and pw

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,19 +1,29 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials";
+import { getUserByEmail } from "prisma/user";
+const bcrypt = require('bcrypt');
+
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email", },
+        password: {  label: "Password", type: "password" }
+      },
+      async authorize( credentials ) {
+        const user = await getUserByEmail(credentials!.email);
+        const validPassword = await bcrypt.compareSync(credentials!.password, user?.passwordHash)
+        return validPassword ? user : null;
+      }
     }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!
+    })
   ]
-  // , callbacks: {
-  //   async jwt({ token }) {
-  //     token.userRole = "admin"
-  //     return token
-  //   },
-  // }
 }
 
 export default NextAuth(authOptions)
