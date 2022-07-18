252878cdb0eb33ff60462a54ffff425d218fe3a8
Rayan Encarnacion <rayanbalbuena@gmail.com>
2022-07-17 22:16:59 -0400

    [A] Toast component, toast css and tailwind css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import type { AppProps } from "next/app";
+import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover
+      />
     </SessionProvider>
   );
 }
