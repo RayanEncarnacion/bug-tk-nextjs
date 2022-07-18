440e3901125dc14a77a509789071dcfa67199d7f
Rayan Encarnacion <rayanbalbuena@gmail.com>
2022-07-17 22:26:30 -0400

    [A] Redirection to index to display toast on success

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -2,9 +2,14 @@ import { signIn } from "next-auth/react";
 
 export default function Login() {
   return (
-    <div>
+    <div className="grid place-content-center h-screen">
       <h1>Login</h1>
-      <button onClick={() => signIn("github", { callbackUrl: "/" })}>
+      <button
+        className="bg-blue-100 text-blue-400 py-1 px-6 outline-none border-0  shadow-sm rounded-md font-bold"
+        onClick={async () =>
+          signIn(undefined, { redirect: false, callbackUrl: "/?toast" })
+        }
+      >
         Signin
       </button>
     </div>
